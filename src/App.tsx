26f109c20bde98a6470b516f81663d7733c9e90d
gpagus2 <agp0004@gmail.com>
2025-02-17 17:50:41 +0100

    Start development of Stadistics and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500">Codemon</h1>
+      <h1 className="text-blue-500 font-press-start-2">Codemon</h1>
     </>
   )
 }
---
+++ src/components/Stadistics.tsx
@@ -0,0 +1,22 @@
+export function Stadistics() {
+  return (
+    <div className="bg-blue-950 text-white py-6 px-4 text-2xl font-mulish font-bold">
+      <div className="max-w-md mx-auto flex justify-between text-center">
+        <div>
+          <p className="text-yellow-400">15</p>
+          <p className="">Usuarios</p>
+        </div>
+
+        <div>
+          <p className="text-yellow-400">190</p>
+          <p className="">Tareas hechas</p>
+        </div>
+
+        <div>
+          <p className="text-yellow-400 ">5050</p>
+          <p className="">Puntos</p>
+        </div>
+      </div>
+    </div>
+  );
+}
