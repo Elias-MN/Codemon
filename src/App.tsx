3f9fc5e78a2712f7c3b7a5b6991e11be6e6a8070
Juanmi <Juanmi26304@gmail.com>
2025-02-18 19:40:32 +0100

    Hero actualizado
    Hero externo creado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
+import Hero from "./components/Hero.tsx";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1 className="text-4xl text-yellow-400 font-press-start-2 filter drop-shadow-[0_4px_0_rgba(0,0,0,1)]">
-          Codemon
-        </h1>
-        <p className="text-xl text-center font-mulish">
-          La mejor forma de empezar a programar: fácil, divertida y accesible.
-        </p>
-        <button className="bg-yellow-400 w-44 h-10 text-base rounded-lg font-press-start-2 shadow-md hover:opacity-60 active:opacity-60 active:shadow-none">
-          <span className="filter drop-shadow-[0_2px_0_rgba(0,0,0,0.3)]">
-            Comenzar
-          </span>
-        </button>
-      </div>
+      <Hero />
     </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,21 @@
-function App() {
+function Hero() {
   return (
     <>
-      <div></div>
-      <h1 className="text-4xl">Codemon</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl text-yellow-400 font-press-start-2 filter drop-shadow-[0_4px_0_rgba(0,0,0,1)]">
+          Codemon
+        </h1>
+        <p className="text-xl text-center font-mulish">
+          La mejor forma de empezar a programar: fácil, divertida y accesible.
+        </p>
+        <button className="bg-yellow-400 w-44 h-10 text-base rounded-lg font-press-start-2 shadow-md hover:opacity-60 active:opacity-60 active:shadow-none">
+          <span className="filter drop-shadow-[0_2px_0_rgba(0,0,0,0.3)]">
+            Comenzar
+          </span>
+        </button>
+      </div>
     </>
   );
 }
 
-export default App;
+export default Hero;
