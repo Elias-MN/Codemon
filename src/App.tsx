74a749ca255d93994d640491418360941c70558c
antonio-munoz55 <altair555mu@gmail.com>
2025-02-20 17:08:21 +0100

    Creating and finalizing the SignUp component
    The Codemon SignUp component has been created correctly, and with all the details

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import RegisterForm from "./components/Signup"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500">Codemon</h1>
+      <RegisterForm></RegisterForm>
     </>
   )
 }
