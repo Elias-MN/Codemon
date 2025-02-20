f6908dcf45542dd6378d72dfc9d948d6021cf146
SantiagoGranado <sgranadop01@iesalbarregas.es>
2025-02-20 17:38:34 +0100

    Finished item
    Complete task list element, all views completed being responsive

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { TaskList } from './components/TaskList.tsx'
+import TasksList from './components/TaskList.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-    <TaskList />
-  </StrictMode>,
+  <>
+    <TasksList />
+  </>,
 )
