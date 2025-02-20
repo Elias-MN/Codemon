28a8aad6f9f7a5d2dec5ea11b4479169d6af37a6
gpagus2 <agp0004@gmail.com>
2025-02-20 17:56:16 +0100

    Increase text size for strong elements
    Increased from md:text-3xl to md:text-4xl

--- src/components/Stadistics.tsx
+++ src/components/Stadistics.tsx
@@ -3,17 +3,17 @@ export function Stadistics() {
     <div className="bg-blue-950 text-white py-6 px-4 text-2xl font-mulish font-bold">
       <div className="flex justify-evenly text-center">
         <div>
-          <strong className="text-yellow-400 md:text-3xl">18</strong>
+          <strong className="text-yellow-400 md:text-4xl">18</strong>
           <p className="text-base md:text-2xl">Usuarios</p>
         </div>
 
         <div>
-          <strong className="text-yellow-400 md:text-3xl">190</strong>
+          <strong className="text-yellow-400 md:text-4xl">190</strong>
           <p className="text-base md:text-2xl">Tareas hechas</p>
         </div>
 
         <div>
-          <strong className="text-yellow-400 md:text-3xl">5050</strong>
+          <strong className="text-yellow-400 md:text-4xl">5050</strong>
           <p className="text-base md:text-2xl">Puntos</p>
         </div>
       </div>
