257b6f6380313052973e300695f8ac63b9e0f7ee
fernandoflores1 <115038723+fernandoflores1@users.noreply.github.com>
2025-02-21 17:06:52 +0100

    Component completed
    All design and responsive requirements implementeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import CalificarTarea from "./components/CalificarTarea"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500">Codemon</h1>
+    <CalificarTarea></CalificarTarea>
     </>
   )
 }
