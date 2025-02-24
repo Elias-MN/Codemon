560f177e193c460ad95259691e2f9e458a7ccbb4
fernandoflores1 <115038723+fernandoflores1@users.noreply.github.com>
2025-02-24 17:26:04 +0100

    Deleted component
    There was deleted the component in App.tsx to evitate future conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import AddTask from "./components/AddTask"
-
 function App() {
-
   return (
     <>
-      <h1 className="text-blue-500">Codemon</h1>
-      <AddTask></AddTask>
+
     </>
   )
 }
 
-export default App
+export default App;
