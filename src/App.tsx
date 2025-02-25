07074184add1bfe171c325a97e128f36506ac4ed
Alvaro-Fuentes04 <fuentesalvaro2004@gmail.com>
2025-02-25 18:08:27 +0100

    Update App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import {UserInfo} from './components/UserInfo.tsx'
-
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500 ">Codemon</h1>
-      <UserInfo/>
+
     </>
   )
 }
