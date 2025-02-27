3d87ff2f5f5d79434f16ab220eff5382c5e66789
gpagus2 <agp0004@gmail.com>
2025-02-27 18:23:48 +0100

    Installed dependencies for router
    With the command:
    
    npm install react-router-dom@latest

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { Footer } from './components/Footer'
 import Info from './components/Info'
 import Hero from './components/Hero';
 import { Stadistics } from './components/Stadistics';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   return (
   <main className='bg-blue-950'>
     <Header />
