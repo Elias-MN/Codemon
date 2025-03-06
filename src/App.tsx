dd2e8afee6e7653589713ac0073b53b56471b20d
EliasMN <elias010493@gmail.com>
2025-03-06 19:14:20 +0100

    Fixhot

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import { Footer } from "./components/Footer";
-import LoginForm  from "./components/Login";
 import RegisterForm  from "./components/Signup";
 import { UserInfo } from "./components/UserInfo";
 
@@ -17,7 +16,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginForm/>} />
         <Route path="/singup" element={<RegisterForm/>} />
         <Route path="/profilepage" element={<UserInfo/>} />
       </Routes>
